7fca500627b878b8f4fff60ea00ff73ec241b5d9
David Obodo <obododavid5@gmail.com>
2020-02-15 14:30:49 +0100

    little refactoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './Reducers/rootReducer' 
+import rootReducer from './Reducers/rootReducer'
 
 const store = createStore(rootReducer);
 
